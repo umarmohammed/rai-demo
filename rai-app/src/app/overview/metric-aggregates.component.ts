276e2f49ced319dbb98f4a1639c2be66e113f69e
Umar Mohammed <umar@thinkincode.co.uk>
2020-06-08 18:27:38 +0100

    use table to display metric aggregates

--- rai-app/src/app/overview/metric-aggregates.component.ts
+++ rai-app/src/app/overview/metric-aggregates.component.ts
@@ -3,8 +3,26 @@ import { OverviewMetric } from '../core/models/overview-metric';
 
 @Component({
   selector: 'rai-metric-aggregates',
-  template: `<div>{{ overviewMetrics | json }}</div>`,
+  template: `<table>
+    <tr>
+      <th>Metric</th>
+      <th>Mean</th>
+      <th>Median</th>
+      <th>CI 5%</th>
+      <th>CI 95%</th>
+      <th>Std</th>
+      <th>Mad</th>
+    </tr>
+    <tr *ngFor="let metric of overviewMetrics">
+      <td>{{ metric.name }}</td>
+      <td *ngFor="let aggregate of metric.aggregates">
+        {{ aggregate.value | number: '1.1-6' }}
+      </td>
+    </tr>
+  </table>`,
+  styleUrls: ['./metric-aggregates.component.scss'],
 })
 export class MetricAggregatesComponent {
   @Input() overviewMetrics: OverviewMetric[];
+  @Input() headings: string[];
 }
