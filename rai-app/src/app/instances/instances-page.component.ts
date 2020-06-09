316f9872182f8ffe08a00993d9835c2e858b77c4
Umar Mohammed <umar@thinkincode.co.uk>
2020-06-09 08:12:47 +0100

    add mat tab nav bar to instances page

--- rai-app/src/app/instances/instances-page.component.ts
+++ rai-app/src/app/instances/instances-page.component.ts
@@ -2,6 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'rai-instances-page',
-  template: `<rai-instances-grid></rai-instances-grid>`,
+  template: ` <nav mat-tab-nav-bar>
+      <a
+        mat-tab-link
+        routerLink="performance"
+        routerLinkActive
+        #rla="routerLinkActive"
+        [active]="rla.isActive"
+      >
+        Performance
+      </a>
+      <a
+        mat-tab-link
+        routerLink="fairness"
+        routerLinkActive
+        #rla1="routerLinkActive"
+        [active]="rla1.isActive"
+      >
+        Fairness
+      </a>
+    </nav>
+    <div style="height: calc(100% - 49px)">
+      <router-outlet></router-outlet>
+    </div>`,
 })
 export class InstancesPageComponent {}
--- rai-app/src/app/instances/instances.module.ts
+++ rai-app/src/app/instances/instances.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { InstancesPageComponent } from './instances-page.component';
 import { Routes, RouterModule } from '@angular/router';
 import { InstancesGridComponent } from './instances-grid.component';
+import { MaterialModule } from '../material/material.module';
 
-const routes: Routes = [{ path: '', component: InstancesPageComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: InstancesPageComponent,
+    children: [
+      { path: 'performance', component: InstancesGridComponent },
+      { path: 'fairness', component: InstancesGridComponent },
+      { path: '', redirectTo: 'performance', pathMatch: 'full' },
+    ],
+  },
+];
 
 @NgModule({
   declarations: [InstancesPageComponent, InstancesGridComponent],
-  imports: [RouterModule.forChild(routes)],
+  imports: [MaterialModule, RouterModule.forChild(routes)],
 })
 export class InstancesModule {}
