3f2254ea9c87852e6c4ba94340a415fd8d991b5f
Umar Mohammed <umar@thinkincode.co.uk>
2020-06-09 08:21:58 +0100

    add instances container component

---
+++ rai-app/src/app/instances/instances-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rai-instances-container',
+  template: `<div>
+    <div>Lime Analysis</div>
+    <rai-instances-grid></rai-instances-grid>
+  </div>`,
+})
+export class InstancesContainerComponent {}
--- rai-app/src/app/instances/instances.module.ts
+++ rai-app/src/app/instances/instances.module.ts
@@ -3,21 +3,26 @@ import { InstancesPageComponent } from './instances-page.component';
 import { Routes, RouterModule } from '@angular/router';
 import { InstancesGridComponent } from './instances-grid.component';
 import { MaterialModule } from '../material/material.module';
+import { InstancesContainerComponent } from './instances-container.component';
 
 const routes: Routes = [
   {
     path: '',
     component: InstancesPageComponent,
     children: [
-      { path: 'performance', component: InstancesGridComponent },
-      { path: 'fairness', component: InstancesGridComponent },
+      { path: 'performance', component: InstancesContainerComponent },
+      { path: 'fairness', component: InstancesContainerComponent },
       { path: '', redirectTo: 'performance', pathMatch: 'full' },
     ],
   },
 ];
 
 @NgModule({
-  declarations: [InstancesPageComponent, InstancesGridComponent],
+  declarations: [
+    InstancesPageComponent,
+    InstancesGridComponent,
+    InstancesContainerComponent,
+  ],
   imports: [MaterialModule, RouterModule.forChild(routes)],
 })
 export class InstancesModule {}
