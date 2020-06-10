3253602f05ae023922be50ffb1901fb2f53a6ddb
Umar Mohammed <umar@thinkincode.co.uk>
2020-06-10 19:01:38 +0100

    add instance difficulty place holder

--- rai-app/src/app/instances/explanation/instance-explanation.component.ts
+++ rai-app/src/app/instances/explanation/instance-explanation.component.ts
@@ -4,6 +4,9 @@ import { Instance } from '../../core/models/instance';
 @Component({
   selector: 'rai-instance-explanation',
   template: `<div class="flex-container">
+    <div class="instance-difficulty">
+      <h3 style="text-align:center;">Instance Difficulty - TBC</h3>
+    </div>
     <rai-instance-probabilities
       [probabilities]="instance.predictProbablities"
     ></rai-instance-probabilities>
