c413f6e478c4a1305586aa1e60d6a5b4875229d0
Umar Mohammed <umar@thinkincode.co.uk>
2020-06-25 11:31:38 +0100

    use mat tab nav bar on adversarial page

--- rai-app/src/app/adversarial/adversarial.module.ts
+++ rai-app/src/app/adversarial/adversarial.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { AdversarialPageComponent } from './adverserial-page.component';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
 
 const routes: Routes = [{ path: '', component: AdversarialPageComponent }];
 
 @NgModule({
   declarations: [AdversarialPageComponent],
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), MaterialModule],
 })
 export class AdversarialModule {}
--- rai-app/src/app/adversarial/adverserial-page.component.ts
+++ rai-app/src/app/adversarial/adverserial-page.component.ts
@@ -2,6 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'rai-adversarial-page',
-  template: `<div>Adversarial Page</div>`,
+  template: `<nav mat-tab-nav-bar>
+      <a
+        mat-tab-link
+        routerLink="model"
+        routerLinkActive
+        #rla="routerLinkActive"
+        [active]="rla.isActive"
+      >
+        Overview
+      </a>
+      <a
+        mat-tab-link
+        routerLink="comparison"
+        routerLinkActive
+        #rla1="routerLinkActive"
+        [active]="rla1.isActive"
+      >
+        Borderlines
+      </a>
+      <a
+        mat-tab-link
+        routerLink="comparison"
+        routerLinkActive
+        #rla1="routerLinkActive"
+        [active]="rla1.isActive"
+      >
+        Inlines
+      </a>
+    </nav>
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>`,
 })
 export class AdversarialPageComponent {}
