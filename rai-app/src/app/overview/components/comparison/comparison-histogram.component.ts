76dd4706adb8270b94e439e9037c1f0778976002
Umar Mohammed <umar@thinkincode.co.uk>
2020-07-01 15:54:29 +0100

    use curve chart for overview comparison

--- rai-app/src/app/overview/components/comparison/comparison-histogram.component.ts
+++ rai-app/src/app/overview/components/comparison/comparison-histogram.component.ts
@@ -1,18 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { Chart, blueScheme, orangeScheme } from 'src/app/core/models/chart';
+import { curveMonotoneX } from 'd3-shape';
 
 @Component({
   selector: 'rai-comparison-histogram',
   template: `
     <div class="container">
-      <ngx-charts-line-chart
+      <ngx-charts-area-chart
         [xAxis]="true"
         [yAxis]="true"
         [results]="lineChartSeries"
         [scheme]="lineChartScheme"
         [legend]="true"
+        [curve]="curve"
+        
       >
-      </ngx-charts-line-chart>
+      </ngx-charts-area-chart>
     </div>
   `,
   styleUrls: ['comparison-histogram.component.scss'],
@@ -20,5 +23,6 @@ import { Chart, blueScheme, orangeScheme } from 'src/app/core/models/chart';
 export class ComparisonHistogramComponent {
   @Input() baselineSeries: Chart[];
   @Input() lineChartSeries: Chart[];
+  curve = curveMonotoneX;
   lineChartScheme = { domain: [blueScheme.domain[0], orangeScheme.domain[0]] };
 }
